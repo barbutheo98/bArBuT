cf7d2e8a11cfcfd3caada08ad891b197d2597e92
barbutheo98 <43355596+barbutheo98@users.noreply.github.com>
2019-10-11 01:40:10 +0100

    Update LCATest.java

--- LCATest.java
+++ LCATest.java
@@ -3,10 +3,21 @@ import static org.junit.Assert.*;
 
 public class LCATest {
     @Test
+    public void createTree(){
+        //test to check if binary tree is correctly initialised
+    }
+
+    @Test
+    public void testNode() {
+        //test to check if node is correctly initialised.
+    }
+
+    @Test
     public void testAncestor() {
         Node n = null;
         assertEquals(false, LCA.found(n, null));
         n = new Node(null, null, null, "root");
         assertEquals(true, LCA.found(n, n));
+        //test to check if ancestor of node with parents
     }
 }
